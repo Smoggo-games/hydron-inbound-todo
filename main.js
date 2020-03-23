53b7057df626e63a891ebdc2b6f39c49f8d5dd5a
Smoggo-games <62225072+Smoggo-games@users.noreply.github.com>
2020-03-23 18:39:05 +1300

    main JavaScript source

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1920,
     height: 1080,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
